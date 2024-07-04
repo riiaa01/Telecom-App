3edf99f6d0cb9caeafd71d608889caab24d4a91d
riiaa01 <91848154+riiaa01@users.noreply.github.com>
2024-07-04 18:12:47 +0530

    tst trial

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ module.exports = {
     secretKey: process.env.SECRET_KEY || "xyz123",
     connectionLink: "mongodb+srv://riya:Riya_12345@cluster0.u0xz4bw.mongodb.net/MyAirtel?retryWrites=true&w=majority",
   };
-  
+  //this is test
